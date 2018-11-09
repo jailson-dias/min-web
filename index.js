fbf620f8f0ee93c5b875501950fc5c80ca8f2a77
João Filipe Moura <jfmrm@cin.ufpe.br>
2018-11-09 18:20:48 -0300

    Sets up docker server api, with express

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
-const fs = require('fs');
-const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+import express from 'express';
+import bodyParser from 'body-parser';
+import http from 'http';
+import { NaturalLanguageUnderstandingV1 } from 'watson-developer-cloud';
 
-var naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+let app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   version: '2018-11-06',
   iam_apikey: 'nWIUdYcZDqxWnqhz3ACTynS5zsZqdI9MlOcS4WexgyCl',
   url: 'https://gateway.watsonplatform.net/natural-language-understanding/api'
 });
 
-var parameters = {
-  'url': 'https://www.endocrino.org.br/10-coisas-que-voce-precisa-saber-sobre-cirurgia-bariatrica/',
-  'features': {
-    'entities': {
-        'model': '9c4ed27b-d7cb-4d64-b73f-80eaab7b939d'
+app.post('/analyze', (req, res) => {
+  const urls = req.body
+
+  const parameters = {
+    'url': 'https://www.endocrino.org.br/10-coisas-que-voce-precisa-saber-sobre-cirurgia-bariatrica/',
+    'features': {
+      'entities': {
+          'model': '9c4ed27b-d7cb-4d64-b73f-80eaab7b939d'
+      }
     }
-  }
-};
+  };
 
-naturalLanguageUnderstanding.analyze(parameters, function(err, response) {
-  if (err) {
-    console.log('erro requisitando a api do watson:', err);
-  } else {
-    fs.writeFile("./output.json", JSON.stringify(response, null, 4), function(err) {
-        if(err) {
-            return console.log("erro salvando o arquivo", err);
-        }
-        console.log("Documento avaliado com sucesso!!");
-    }); 
-  }
-
+  naturalLanguageUnderstanding.analyze(parameters, (error, response) => {
+    if (!error) {
+      res.status(200).json(response);
+    } else {
+      console.log(error);
+      res.status(500).json({ message: 'internal server error' });
+    }
+  })
 });
+
+const port = app.get('port');
+http.createServer(app).listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
+
+var urls = [
+  ['https://www.endocrino.org.br/10-coisas-que-voce-precisa-saber-sobre-diabetes/', 'diabetes'],
+  ['https://www.endocrino.org.br/10-coisas-que-voce-precisa-saber-sobre-obesidade/', 'obesidade'],
+  ['https://www.endocrino.org.br/10-coisas-que-voce-precisa-saber-sobre-hipertensao/', 'hipertensao'],
+  ['https://www.tuasaude.com/diabetes-tipo-2/', 'diabetes'],
+  ['https://www.tuasaude.com/o-que-comer-na-diabetes/', 'alimentacao'],
+  ['http://prevencao.cardiol.br/fatores-de-risco/hipertensao.asp', 'hipertensao'] 
+]
