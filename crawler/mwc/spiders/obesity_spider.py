712b1ccac8b00b6d88e9214f1441b99ff05f30a8
Arthurlpgc <arthurlpgcosta@gmail.com>
2018-12-09 19:20:10 -0300

    filters

--- crawler/mwc/spiders/obesity_spider.py
+++ crawler/mwc/spiders/obesity_spider.py
@@ -1,90 +1,90 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import scrapy, json
+import scrapy, json, re
 
-terms = { 
-    'diabetes': 'glicose',
-    'hipertensao': 'pressao-aeterial',
-    'hipertensao': 'hipertensão',
-    'alimentacao': 'acucar',
-    'alimentacao': 'gordura',
-    'cirurgiaBariatrica': 'peso',
-    'cirurgiaBariatrica': 'Obesidade',
-    'cirurgiaBariatrica': 'cirurgia-bariatrica',
-    'hipertensao': 'coração',
-    'diabetes': 'pancreas',
-    'cirurgiaBariatrica': 'estomago',
-    'hipertensao': 'colesterol',
-    'alimentacao': 'gastrite',
-    'hipertensao': 'doenca-cardio-vascular',
-    'hipertensao': 'cardiaco',
-    'cirurgiaBariatrica': 'morbidade',
-    'cirurgiaBariatrica': 'morbido',
-    'cirurgiaBariatrica': 'Gordo',
-    'alimentacao': 'triglicerídeo',
-    'alimentacao': 'fígado',
-    'cirurgiaBariatrica': 'alcoolismo',
-    'diabetes': 'insulina',
-    'cirurgiaBariatrica': 'Imc',
-    'alimentacao': 'comida',
-    'alimentacao': 'alimentacao-saudavel',
-    'alimentacao': 'frutas',
-    'alimentacao': 'hdl',
-    'alimentacao': 'ldl',
-    'cirurgiaBariatrica': 'bariátrica',
-    'cirurgiaBariatrica': 'estomago',
-    'cirurgiaBariatrica': 'apneia',
-    'cirurgiaBariatrica': 'Peso',
-    'alimentacao': 'Strogonoff',
-    'alimentacao': 'Lasanha',
-    'alimentacao': 'Macarronada',
-    'alimentacao': 'Fruta',
-    'cirurgiaBariatrica': 'sobrepeso',
-    'alimentacao': 'Aveia',
-    'alimentacao': 'Fibras', 
-    'alimentacao': 'Cereais', 
-    'alimentacao': 'Verdura', 
-    'alimentacao': 'Proteína',
-    'alimentacao': 'Carboidrato',
-    'alimentacao': 'Dieta',
-    'cirurgiaBariatrica': 'Hérnia',
-    'cirurgiaBariatrica': 'Laparoscopia',
-    'cirurgiaBariatrica': 'dor-nas-juntas',
-    'hipertensao': 'Infarto',
-    'hipertensao': 'coronaria',
-    'cirurgiaBariatrica': 'gastrico',
-    'cirurgiaBariatrica': 'Gastro',
-    'alimentacao': 'Nutricionista', 
-    'hipertensao': 'Cardiologista',
-    'alimentacao': 'Granola',
-    'alimentacao': 'Manga', 
-    'alimentacao': 'Cha-verde',
-    'alimentacao': 'Emagrece',
-    'alimentacao': 'Chá preto',
-    'alimentacao': 'Chia',
-    'alimentacao': 'Lichia',
-    'alimentacao': 'Jamelão',
-    'alimentacao': 'Aveia',
-    'diabetes':'Glicosimetro',
-    'hipertensao': 'Esfigmomanometro',
-    'cirurgiaBariatrica': 'Artrose',
-    'cirurgiaBariatrica': 'Fertilidade',
-    'cirurgiaBariatrica': 'Sexo',
-    'cirurgiaBariatrica': 'Osteoporose',
-    'cirurgiaBariatrica':'Entalamento',
-    'cirurgiaBariatrica': 'Entalar',
-    'cirurgiaBariatrica':'Nausea',
-    'cirurgiaBariatrica': 'Vomito',
-    'diabetes': 'Hemoglobina glicada',
-    'diabetes': 'Hiperglicemia',
-    'cirurgiaBariatrica': 'Hipoglicemia',
-    'diabetes': 'Pre-diabetico',
-    'diabetes': 'Diabetico',
-    'cirurgiaBariatrica': 'Cigarro',
-    'cirurgiaBariatrica': 'Fumo',
-    'cirurgiaBariatrica':'Tabagismo' 
-}
+terms = [
+'glicose',
+'pressao-aeterial',
+'hipertensão',
+'acucar',
+'gordura',
+'peso',
+'Obesidade',
+'cirurgia-bariatrica',
+'coração',
+'pancreas',
+'estomago',
+'colesterol',
+'gastrite',
+'doenca-cardio-vascular',
+'cardiaco',
+'morbidade',
+'morbido',
+'Gordo',
+'triglicerídeo',
+'fígado',
+'alcoolismo',
+'insulina',
+'Imc',
+'comida',
+'alimentacao-saudavel',
+'frutas',
+'hdl',
+'ldl',
+'bariátrica',
+'estomago',
+'apneia',
+'Peso',
+'Strogonoff',
+'Lasanha',
+'Macarronada',
+'Fruta',
+'sobrepeso',
+'Aveia',
+'Fibras', 
+'Cereais', 
+'Verdura', 
+'Proteína',
+'Carboidrato',
+'Dieta',
+'Hérnia',
+'Laparoscopia',
+'dor-nas-juntas',
+'Infarto',
+'coronaria',
+'gastrico',
+'Gastro',
+'Nutricionista', 
+'Cardiologista',
+'Granola',
+'Manga', 
+'Cha-verde',
+'Emagrece',
+'Chá preto',
+'Chia',
+'Lichia',
+'Jamelão',
+'Aveia',
+'Glicosimetro',
+'Esfigmomanometro',
+'Artrose',
+'Fertilidade',
+'Sexo',
+'Osteoporose',
+'Entalamento',
+'Entalar',
+'Nausea',
+'Vomito',
+'Hemoglobina glicada',
+'Hiperglicemia',
+'Hipoglicemia',
+'Pre-diabetico',
+'Diabetico',
+'Cigarro',
+'Fumo',
+'Tabagismo' 
+]
 
 class ObesitySpider(scrapy.Spider):
     name = 'obesity'
@@ -98,26 +98,35 @@ class ObesitySpider(scrapy.Spider):
         'https://www.tuasaude.com/diabetes-tipo-2/',
     ]
 
-    def save(self, response):
-        file = open("/src/pages/list", 'a')
-        file.write(response.url + '\n')
-        file.close()
-        domain = response.url.split('/')[2]
+    def get_text(self, response):
         text = ''
         paragraphs = response.xpath("//p")
         for p in paragraphs:
             paragraph = p.xpath("text()").extract_first()
             if paragraph != None:
                 text = text + paragraph + '\n'
+        return text
+
+    def save(self, response):
+        file = open("/src/pages/list", 'a')
+        file.write(response.url + '\n')
+        file.close()
+        domain = response.url.split('/')[2]
         file = open("/src/pages/jsons/" + response.url.replace('/', '_') + '.json', 'w')
-        data = {"url": response.url, "text": text}
+        data = {"url": response.url, "text": self.get_text(response)}
         file.write(json.dumps(data))
         file.close()
 
     def is_article(self, response):
         if response.url.split('/')[2] in ['saudebemestar.com.pt', 'www.tuasaude.com', 'www.endocrino.org.br']:
             if response.url.count('/') == 4 and response.url.count('?') == 0:
-                return True
+                ret = False
+                words = re.sub("[^\w]", " ",  self.get_text(response)).split()
+                for uword in words:
+                    word = uword.lower()
+                    if word in terms:
+                        ret = True
+                return ret
         return False
 
     def parse(self, response):
