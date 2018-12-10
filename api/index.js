80f6f3e8f8938c3373f63f0965aacc613af566c8
João Filipe Moura <jfmrm@cin.ufpe.br>
2018-12-10 15:59:31 -0300

    Takes out watson credentials

--- api/index.js
+++ api/index.js
@@ -14,29 +14,29 @@ app.use(bodyParser.json());
 
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   version: '2018-11-06',
-  iam_apikey: 'nWIUdYcZDqxWnqhz3ACTynS5zsZqdI9MlOcS4WexgyCl',
-  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api'
+  iam_apikey: '',
+  url: ''
 });
 
 const naturalLanguageClassifier = new NaturalLanguageClassifierV1({
-  iam_apikey: 'BewA4lI45fs9zYRQt3oqdp95x8LZoSMbhYiDleJnDa8b',
-  url: 'https://gateway.watsonplatform.net/natural-language-classifier/api'
+  iam_apikey: '',
+  url: ''
 });
 
 const discovery = new DiscoveryV1({
   version: '2018-12-03',
-  iam_apikey: 'ta7yULFv6MXTOSR9XbMN4GiTUd3gDkW5_iukw362UqEd',
-  url: 'https://gateway.watsonplatform.net/discovery/api'
+  iam_apikey: '',
+  url: ''
 });
 
 app.post('/analyze', (req, res) => {
   const urls = req.body
 
   const parameters = {
-    'url': 'https://www.endocrino.org.br/10-coisas-que-voce-precisa-saber-sobre-cirurgia-bariatrica/',
+    'url': '',
     'features': {
       'entities': {
-          'model': '9c4ed27b-d7cb-4d64-b73f-80eaab7b939d'
+          'model': ''
       }
     }
   };
@@ -56,7 +56,7 @@ app.post('/classify', (req, res) => {
 
   naturalLanguageClassifier.classify({
     text,
-    classifier_id: 'cd1030x458-nlc-860' },
+    classifier_id: '' },
     function(err, response) {
       if (err) {
         console.log('error:', err);
@@ -69,9 +69,9 @@ app.post('/classify', (req, res) => {
 
 app.post('/sumarize', (req, res) => {
   discovery.query({
-    environment_id: 'e1d0e813-5dd8-450b-94c0-a16c729ae0c6',
-    collection_id: '650d3842-4df7-4064-a0f1-b4900ebbe0d1',
-    natural_language_query: 'qual a parte mais relevante do texto?'
+    environment_id: '',
+    collection_id: '',
+    natural_language_query: 'qual a parte mais relevante do texto?',
   }, (err, response) => {
     if (err) {
       console.log('error: ', err);
